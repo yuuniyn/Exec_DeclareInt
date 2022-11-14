0044602756ffdba768764b3f5a1dc70f40a49c30
Yun ni <erknard2015@gmail.com>
2022-11-14 23:35:39 +0800

    Revise

--- Program.cs
+++ Program.cs
@@ -6,13 +6,31 @@ using System.Threading.Tasks;
 
 namespace Exec_DeclareInt
 {
-   internal class Program
+    internal class Program
     {
         static void Main(string[] args)
         {
             ////計算1+(2+2)*3-1
             int result = 1 + (2 + 2) * 3 - 1;
-            Console.WriteLine(result);       
+            Console.WriteLine(result);
+
+            //120/7的商以及餘數是多少
+            int A = 120;
+            int B = 7;
+            int result1 = A / B;
+            int result2 = A % B;
+            Console.WriteLine(result1);
+            Console.WriteLine(result2);
+
+
+            {//判斷9,202各數為奇偶數?
+                int result3 = 9 % 2;
+                int result4 = 202 % 2;
+
+                Console.WriteLine(result3);
+                Console.WriteLine(result4);
+            }
+
         }
     }
-}
+} 
