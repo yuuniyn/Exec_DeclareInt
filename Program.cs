e8abc6548f0f9bdfb762c71461e9698b4ab1d03d
User <User@USER-PC>
2022-11-14 23:25:27 +0800

    加入專案檔案。

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Exec_DeclareInt
+{
+   internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ////計算1+(2+2)*3-1
+            int result = 1 + (2 + 2) * 3 - 1;
+            Console.WriteLine(result);       
+        }
+    }
+}
